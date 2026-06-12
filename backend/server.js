f39ea8e8b3bd1312e8aab26153e651024a52975b
KasunV-git <vinod.kasun23@gmail.com>
2026-06-13 01:17:53 +0530

    Implement authentication routes and middleware structure

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
+
 app.get("/", (req, res) => {
   res.send("Sphere API Running");
 });
@@ -16,3 +19,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.use("/api/auth", require("./routes/authRoutes"));
